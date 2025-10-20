86bc179b4eaa74249c58d29ea989b40230097d5e
rexx <coderedem1500@gmail.com>
2025-10-20 10:38:03 +0700

    rawr

---
+++ day_1/main.py
@@ -0,0 +1,10 @@
+print("Halo Septiyan Bintang Ramadhan Putra")
+
+if 10 > 5:
+    print("10 lebih besar dari 5")
+
+name = input("Masukkan nama Anda : ")
+print(f"Halo {name}")
+
+address = input("Masukkan alamat rumah Anda : ")
+print("Alamat rumah Anda adalah " + address)
